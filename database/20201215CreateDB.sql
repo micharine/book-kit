2a1a62bf1afca20384d6fc32f018500ae1931117
Michelle Hansey <6622259+micharine@users.noreply.github.com>
2020-12-16 00:48:56 -0800

    wip: create database

---
+++ database/20201215CreateDB.sql
@@ -0,0 +1,22 @@
+CREATE SCHEMA `bookkit_dev` ;
+-- Inventory Item Table
+CREATE TABLE `bookkit_dev`.`inventoryitem` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` CHAR(255) NULL,
+  `description` CHAR(255) NULL,
+  `cost` DECIMAL NULL,
+  `code` CHAR(255) NULL,
+  `quantityInStock` INT NULL,
+  `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
+
+-- Order Table
+CREATE TABLE `bookkit_dev`.`order` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `inventoryItemCode` CHAR(255) NULL,
+  `quantityOrdered` INT NULL,
+  `customerID` CHAR(255) NULL,
+  `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+  `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
