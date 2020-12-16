7e87323ec52c24391de478def9e7d2eb448f4f86
Michelle Hansey <6622259+micharine@users.noreply.github.com>
2020-12-16 02:29:53 -0800

    wip: start adding inventory

---
+++ database/20201216AddInventory.sql
@@ -0,0 +1,7 @@
+INSERT INTO `bookkit_dev`.`inventoryitem` (`name`, `description`, `code`, `quantityInStock`) VALUES ('Genesis', 'book, study guide, poster, journal', 'gen', '30');
+INSERT INTO `bookkit_dev`.`inventoryitem` (`name`, `code`, `quantityInStock`) VALUES ('Exodus', 'exo', '32');
+INSERT INTO `bookkit_dev`.`inventoryitem` (`name`, `code`, `quantityInStock`) VALUES ('Leviticus', 'lev', '25');
+INSERT INTO `bookkit_dev`.`inventoryitem` (`name`, `code`, `quantityInStock`) VALUES ('Numbers', 'num', '0');
+INSERT INTO `bookkit_dev`.`inventoryitem` (`name`, `code`, `quantityInStock`) VALUES ('Deuteronomy', 'deut', '45');
+INSERT INTO `bookkit_dev`.`inventoryitem` (`name`, `code`, `quantityInStock`) VALUES ('Joshua', 'josh', '15');
+INSERT INTO `bookkit_dev`.`inventoryitem` (`name`, `code`, `quantityInStock`) VALUES ('Judges', 'jdg', '43');
